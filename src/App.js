57f4e1f350853af1dd0f4c30f3bde0f2dcc1b58a
Mihajlo Mitic <mihajlomiticmiki@gmail.com>
2024-10-10 18:25:11 +0200

    refactor: code format using prittier formatter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar.js';
-import Home from './components/Home.js';
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
-import { blue, indigo } from '@mui/material/colors';
-import { useState } from 'react';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.js";
+import Home from "./components/Home.js";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { blue, indigo } from "@mui/material/colors";
+import { useState } from "react";
 
 //TODO: https://mui.com/material-ui/customization/color/
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
   },
 });
 
 const lightTheme = createTheme({
   palette: {
     primary: {
-      light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#002884',
-      contrastText: '#fff',
+      light: "#757ce8",
+      main: "#3f50b5",
+      dark: "#002884",
+      contrastText: "#fff",
     },
     secondary: indigo,
   },
@@ -28,21 +27,20 @@ const lightTheme = createTheme({
 
 function App() {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
-  function handleThemeClick(){
-      setIsDarkTheme(!isDarkTheme);
+  function handleThemeClick() {
+    setIsDarkTheme(!isDarkTheme);
   }
-  
+
   return (
     <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
       <CssBaseline />
 
       <Router>
-        <Navbar  handleThemeClick={handleThemeClick}/>
+        <Navbar handleThemeClick={handleThemeClick} />
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
-    
     </ThemeProvider>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,99 +1,162 @@
-import { AppBar, Button, Collapse, IconButton, List, ListItem, ListItemButton, styled, Toolbar, Typography } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
+import {
+  AppBar,
+  Button,
+  Collapse,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import { Contrast } from '@mui/icons-material';
+import { Contrast } from "@mui/icons-material";
 
 const NavbarButton = styled(Button)({
-    textTransform: 'none',
-    fontSize: 14,
+  textTransform: "none",
+  fontSize: 14,
 });
 
-const navElements = ['Home', 'Education', 'Experience', 'Projects', 'Skills', 'About', 'Contact'];
+const navElements = [
+  "Home",
+  "Education",
+  "Experience",
+  "Projects",
+  "Skills",
+  "About",
+  "Contact",
+];
 
-function Navbar({handleThemeClick}) {
-    const small = useMediaQuery("(max-width:719px)");
-    const full = useMediaQuery("(min-width:720px)");
+function Navbar({ handleThemeClick }) {
+  const small = useMediaQuery("(max-width:719px)");
+  const full = useMediaQuery("(min-width:720px)");
 
-    const [open, setOpen] = useState(false);
-    const handleClick = () => {
-        setOpen(!open);
-    }
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    setOpen(!open);
+  };
 
-    return (<div>
-        <AppBar position="fixed">
-            <Toolbar >
-                {small && (
-                    <>
-                        <List 
-                            sx={{
-                                width: '100%'
-                            }}>
-                            <ListItem disableGutters disablePadding>                    
-                                <Typography variant="h6"
-                                    sx={{ flexGrow: 1, textDecoration: "none", boxShadow: "none", color: "inherit" }} 
-                                    component={Link} to="/"
-                                >
-                                    Mihajlo Mitić
-                                </Typography>
-                                <ListItemButton disableGutters
-                                    edge="end"
-                                    onClick={handleClick}
-                                    color="inherit"
-                                    sx={{maxWidth:'25px', pr:'50px'}}>
-                                        <MenuIcon fontSize='medium'/>
-                                </ListItemButton>
-                                <ListItemButton disableGutters
-                                    edge="end"
-                                    color='inherit' 
-                                    onClick={handleThemeClick}
-                                    sx={{maxWidth:'25px'}}>
-                                    <Contrast fontSize='small'></Contrast>
-                                </ListItemButton>
-                            </ListItem>
-                            <ListItem disableGutters disablePadding> 
-                                <Collapse
-                                    in = {open}
-                                    timeout = "auto"
-                                    unmountOnExit
-                                >
-                                    <List>
-                                        {navElements.map((el) => (    
-                                            <ListItem key={el} disablePadding>
-                                                {el == 'Home' ? 
-                                                <ListItemButton color='inherit' component={Link} to='/' onClick={handleClick}>{el}</ListItemButton>:
-                                                <ListItemButton color='inherit' component={Link} to={'/'+ el.toLowerCase()} onClick={handleClick}>{el}</ListItemButton>}
-                                            </ListItem>
-                                        ))}
-                                    </List>
-                                </Collapse>
-                            </ListItem>
-                        </List>
-                    </>
-                )}
+  return (
+    <div>
+      <AppBar position="fixed">
+        <Toolbar>
+          {small && (
+            <>
+              <List
+                sx={{
+                  width: "100%",
+                }}
+              >
+                <ListItem disableGutters disablePadding>
+                  <Typography
+                    variant="h6"
+                    sx={{
+                      flexGrow: 1,
+                      textDecoration: "none",
+                      boxShadow: "none",
+                      color: "inherit",
+                    }}
+                    component={Link}
+                    to="/"
+                  >
+                    Mihajlo Mitić
+                  </Typography>
+                  <ListItemButton
+                    disableGutters
+                    edge="end"
+                    onClick={handleClick}
+                    color="inherit"
+                    sx={{ maxWidth: "25px", pr: "50px" }}
+                  >
+                    <MenuIcon fontSize="medium" />
+                  </ListItemButton>
+                  <ListItemButton
+                    disableGutters
+                    edge="end"
+                    color="inherit"
+                    onClick={handleThemeClick}
+                    sx={{ maxWidth: "25px" }}
+                  >
+                    <Contrast fontSize="small"></Contrast>
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disableGutters disablePadding>
+                  <Collapse in={open} timeout="auto" unmountOnExit>
+                    <List>
+                      {navElements.map((el) => (
+                        <ListItem key={el} disablePadding>
+                          {el == "Home" ? (
+                            <ListItemButton
+                              color="inherit"
+                              component={Link}
+                              to="/"
+                              onClick={handleClick}
+                            >
+                              {el}
+                            </ListItemButton>
+                          ) : (
+                            <ListItemButton
+                              color="inherit"
+                              component={Link}
+                              to={"/" + el.toLowerCase()}
+                              onClick={handleClick}
+                            >
+                              {el}
+                            </ListItemButton>
+                          )}
+                        </ListItem>
+                      ))}
+                    </List>
+                  </Collapse>
+                </ListItem>
+              </List>
+            </>
+          )}
 
-                {full && (
-                    <>                    
-                        <Typography variant="h6" 
-                            sx={{ flexGrow: 1, textDecoration: "none", boxShadow: "none", color: "inherit" }} 
-                            component={Link} to="/"
-                        >
-                            Mihajlo Mitić
-                        </Typography>
+          {full && (
+            <>
+              <Typography
+                variant="h6"
+                sx={{
+                  flexGrow: 1,
+                  textDecoration: "none",
+                  boxShadow: "none",
+                  color: "inherit",
+                }}
+                component={Link}
+                to="/"
+              >
+                Mihajlo Mitić
+              </Typography>
 
-                        {navElements.map((el) => (
-                            el == 'Home' ? 
-                            <NavbarButton color='inherit' component={Link} to='/'>{el}</NavbarButton>:
-                            <NavbarButton color='inherit' component={Link} to={'/'+ el.toLowerCase()}>{el}</NavbarButton>
-                        ))}
-                        <IconButton color='inherit' onClick={handleThemeClick}><Contrast fontSize='small'></Contrast></IconButton>
-                    </>
-                )}
-
-            </Toolbar>
-        </AppBar></div>
-    );
-};
+              {navElements.map((el) =>
+                el == "Home" ? (
+                  <NavbarButton color="inherit" component={Link} to="/">
+                    {el}
+                  </NavbarButton>
+                ) : (
+                  <NavbarButton
+                    color="inherit"
+                    component={Link}
+                    to={"/" + el.toLowerCase()}
+                  >
+                    {el}
+                  </NavbarButton>
+                )
+              )}
+              <IconButton color="inherit" onClick={handleThemeClick}>
+                <Contrast fontSize="small"></Contrast>
+              </IconButton>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
 
 export default Navbar;
