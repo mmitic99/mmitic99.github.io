54e292fd786cd649c0dabb60fd92348d51825dd8
Mihajlo Mitic <mihajlomiticmiki@gmail.com>
2024-10-14 17:56:42 +0200

    feat: home page content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,91 +1,59 @@
-import { Box } from "@mui/material";
+import {
+  GitHub,
+  Google,
+  Instagram,
+  LinkedIn,
+  Twitter,
+} from "@mui/icons-material";
+import { Box, IconButton, Paper, Typography } from "@mui/material";
 import React from "react";
 
 const Home = () => {
   return (
-    <Box sx={{ m: 3, mt: 10.5 }}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-      bibendum, magna vitae mattis feugiat, sem dui ornare nulla, et porttitor
-      lectus dolor quis tellus. Ut pulvinar feugiat venenatis. Cras nec dictum
-      lectus. Sed interdum a risus a tempus. Sed ultrices semper eros tincidunt
-      egestas. Vestibulum quis leo ultricies, iaculis turpis sollicitudin,
-      mollis felis. Integer accumsan congue eros, nec tempus ipsum. Vestibulum
-      non ipsum non leo ultricies dignissim nec vel mauris. Fusce nec orci
-      lobortis, vehicula sem vitae, luctus nisi. Proin finibus ac lacus ut
-      fermentum. Nam eget hendrerit velit, ac molestie metus. Phasellus varius
-      ante nisi, in ornare nunc pulvinar a. Nulla facilisi. Duis congue eget
-      sapien ac sagittis. Donec dapibus luctus dignissim. Donec mollis urna id
-      sem euismod, eget dictum erat finibus. Integer a odio in tortor pharetra
-      pulvinar ac id sapien. Sed pretium tempus mi, ut sodales velit venenatis
-      vitae. Duis non efficitur eros. Donec interdum ex eu eros convallis, sit
-      amet commodo felis placerat. Integer lacinia nibh vel odio tristique
-      imperdiet. Nam quis magna sit amet felis efficitur tincidunt sed nec
-      purus. Donec egestas urna ligula, sed maximus nunc sollicitudin sed. Donec
-      tortor dui, gravida et ornare nec, molestie eget elit. Etiam eu orci
-      tellus. Fusce sollicitudin semper rutrum. Pellentesque vulputate nisl eget
-      turpis facilisis sollicitudin. Class aptent taciti sociosqu ad litora
-      torquent per conubia nostra, per inceptos himenaeos. Sed in purus quam.
-      Quisque ultricies dolor at ante finibus pharetra. Sed at dapibus urna. Sed
-      egestas, ipsum eget blandit dictum, odio nisi facilisis mauris, id commodo
-      turpis risus at nunc. Suspendisse vulputate nibh a sem pulvinar
-      sollicitudin. Fusce consequat risus nisl, ultricies luctus augue gravida
-      vel. Aliquam erat volutpat. Aliquam varius turpis vitae lacus mollis
-      placerat. Phasellus arcu orci, feugiat ut nisi et, mattis pulvinar ipsum.
-      Nunc dictum est vel egestas sodales. Nam tristique lacus urna, non commodo
-      sem placerat et. Curabitur pellentesque lacinia ornare. Mauris consequat
-      purus ut turpis volutpat, vitae faucibus lorem dapibus. Fusce rutrum erat
-      quis ligula ornare laoreet. Suspendisse pharetra magna nec mi tincidunt,
-      eleifend hendrerit nisl vestibulum. Vestibulum interdum libero id enim
-      vulputate vulputate. Curabitur ultricies finibus diam quis gravida. Etiam
-      et hendrerit magna. Quisque pretium gravida enim in tincidunt. Nunc urna
-      libero, suscipit eget mi eu, volutpat porta arcu. Etiam volutpat dapibus
-      ante, vel vulputate tellus gravida volutpat. In tincidunt facilisis augue,
-      sed mollis ipsum commodo quis. Integer porta cursus scelerisque. Vivamus
-      dignissim elit id maximus fringilla. Nam quis tincidunt est. Aliquam vel
-      pharetra erat, ac lacinia felis. Nulla sodales vel lacus vitae blandit.
-      Nullam molestie nulla non neque tempor varius. Aliquam erat volutpat.
-      Quisque vel pellentesque ligula. Fusce ac turpis dictum, scelerisque purus
-      eget, congue massa. Curabitur quis mauris nec neque hendrerit dapibus quis
-      eget nisi. Quisque tristique porta hendrerit. Suspendisse non arcu quam.
-      Suspendisse eu justo tortor. Curabitur et odio sed tortor dapibus
-      tincidunt. Cras aliquam a purus vel interdum. Phasellus lorem mauris,
-      convallis at arcu quis, iaculis egestas massa. Vestibulum ante ipsum
-      primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam
-      interdum velit sed urna tempor gravida. Integer varius semper ex, eget
-      consectetur augue ornare eu. Nullam in gravida arcu. Suspendisse
-      scelerisque gravida suscipit. Praesent nec ligula vestibulum leo suscipit
-      bibendum. Ut sodales lacus sed interdum sollicitudin. Integer nec faucibus
-      neque. Aenean turpis est, molestie et vestibulum eu, interdum at dui. Ut
-      accumsan ex at faucibus dictum. Mauris risus est, ultrices at turpis
-      vitae, viverra molestie risus. Nulla vestibulum sagittis iaculis. Morbi ac
-      orci quis nulla eleifend placerat elementum non mi. Fusce augue lectus,
-      faucibus quis quam id, bibendum scelerisque tellus. Phasellus eget metus
-      et dui tristique lacinia non sed arcu. Nulla condimentum vehicula pretium.
-      Nulla ac lorem nec ante facilisis vestibulum. Interdum et malesuada fames
-      ac ante ipsum primis in faucibus. Nulla facilisi. Sed lacinia velit dui,
-      id rhoncus ligula interdum et. Curabitur consectetur semper elit ac porta.
-      Morbi non nulla ex. Sed aliquam metus non facilisis lacinia. Proin
-      scelerisque mi quis eros molestie, et ultricies ligula accumsan. Morbi
-      ultrices quam vel efficitur egestas. Sed malesuada tortor vel nulla
-      scelerisque ultricies. Praesent at odio luctus, iaculis velit et,
-      facilisis leo. Donec finibus sed libero eu aliquam. Etiam aliquam lorem
-      imperdiet nulla molestie, sit amet mattis ex lobortis. Morbi tincidunt
-      nulla ac lacus lacinia consequat. In hac habitasse platea dictumst.
-      Vivamus sagittis ipsum velit, at dignissim neque gravida id. Maecenas a
-      tempus quam. Maecenas vel fermentum neque, vel feugiat libero. Proin et
-      cursus sapien. Phasellus laoreet, nunc rutrum tincidunt euismod, ipsum
-      libero pretium nulla, in rhoncus mi erat sit amet metus. Curabitur euismod
-      tortor odio, at semper metus aliquam eget. Nam in lacus quis lacus
-      consectetur porttitor non nec turpis. Nulla suscipit faucibus est quis
-      pellentesque. Praesent eleifend iaculis nunc, sit amet sollicitudin sapien
-      varius vel. Morbi venenatis tortor at porttitor cursus. Nullam sed est
-      quis magna iaculis mollis. Nunc molestie leo id risus commodo, id
-      convallis sem porttitor. Integer mattis massa lorem, lacinia imperdiet
-      velit ultricies eget. Donec facilisis pellentesque viverra. Sed iaculis,
-      nunc pulvinar mollis blandit, sem velit tristique sem, ac aliquam est
-      tellus non erat. Nullam malesuada dui ornare mauris rhoncus, at commodo
-      metus malesuada. Mauris et quam sit amet purus vehicula aliquet non eu
-      odio. Nullam efficitur vehicula nunc, dictum sodales justo rhoncus vitae.
+    <Box>
+      <Paper>
+        <IconButton>
+          <GitHub fontSize="small"></GitHub>
+        </IconButton>
+        <IconButton>
+          <LinkedIn fontSize="small"></LinkedIn>
+        </IconButton>
+        <IconButton>
+          <Instagram fontSize="small"></Instagram>
+        </IconButton>
+        <IconButton>
+          <Google fontSize="small"></Google>
+        </IconButton>
+      </Paper>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        sx={{
+          m: "12vw",
+          mt: 10.5,
+          minHeight: "70vh",
+        }}
+      >
+        <Paper elevation={0}>
+          <Typography
+            variant="h4"
+            gutterBottom
+            color="inherit"
+            align="center"
+            sx={{ pb: "2vh" }}
+          >
+            Hey, I'm Mihajlo Mitić
+          </Typography>
+          <Typography variant="body2" align="center" sx={{ p: 0 }}>
+            I am a motivated and detail-oriented software developer with a
+            Bachelor's degree in Electrical and Computer Engineering. Passionate
+            about technology and problem-solving, I am eager to begin a
+            rewarding career in the tech industry by applying my skills and
+            continuously learning.
+          </Typography>
+        </Paper>
+      </Box>
     </Box>
   );
 };
