936370e2359b8acf20e329c2624ac466a904abf0
Mihajlo Mitic <mihajlomiticmiki@gmail.com>
2024-10-25 13:33:24 +0200

    feat: added home page image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,60 @@
-import { Typography } from "@mui/material";
+import { Grid2, Typography, useMediaQuery } from "@mui/material";
 import React from "react";
 import SocialMediaBar from "./SocialMediaBar";
 import TransparentPaper from "./common/TransparentPaper";
 import CenteredBox from "./common/CenteredBox";
+import { smallScreen } from "./common/const";
 
 function Home() {
+  const small = useMediaQuery(smallScreen);
   return (
     <CenteredBox>
       <TransparentPaper>
-        <Typography
-          variant="h4"
-          gutterBottom
-          color="inherit"
-          align="center"
-          sx={{ pb: "2vh" }}
+        <Grid2
+          container
+          spacing={small ? 0 : 2}
+          columns={16}
+          direction={small ? "column" : "row"}
+          sx={{
+            alignItems: "center",
+            justify: "flex-end",
+          }}
         >
-          Hey, I'm Mihajlo Mitić
-        </Typography>
-        <Typography variant="body2" align="center" sx={{ p: 0 }}>
-          I am a motivated and detail-oriented software developer with a
-          Bachelor's degree in Electrical and Computer Engineering. Passionate
-          about technology and problem-solving, I am eager to begin a rewarding
-          career in the tech industry by applying my skills and continuously
-          learning.
-        </Typography>
+          <Grid2
+            size={small ? 16 : 8}
+            alignItems="center"
+            justify="flex-end"
+            order={small ? 2 : 1}
+          >
+            <img
+              src={process.env.PUBLIC_URL + "/img/home.png"}
+              style={{ width: "100%", height: "100%" }}
+            />
+          </Grid2>
+          <Grid2
+            size={small ? 16 : 8}
+            alignItems="center"
+            justify="center"
+            order={small ? 1 : 2}
+          >
+            <Typography
+              variant="h4"
+              gutterBottom
+              color="inherit"
+              align="center"
+              sx={{ pb: "2vh" }}
+            >
+              Hey, I'm Mihajlo Mitić
+            </Typography>
+            <Typography variant="body2" align="center" sx={{ p: 0 }}>
+              I am a motivated and detail-oriented software developer with a
+              Bachelor's degree in Electrical and Computer Engineering.
+              Passionate about technology and problem-solving, I am eager to
+              begin a rewarding career in the tech industry by applying my
+              skills and continuously learning.
+            </Typography>
+          </Grid2>
+        </Grid2>
       </TransparentPaper>
       <SocialMediaBar></SocialMediaBar>
     </CenteredBox>
